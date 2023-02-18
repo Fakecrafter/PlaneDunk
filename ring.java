7059e71459debf0d9e8b0ab6a658168f5dec20df
Fakecrafter <Fakecrafter@protonmail.com>
2023-02-18 19:35:24 +0100

    added basketball color

--- ring.java
+++ ring.java
@@ -7,6 +7,7 @@ class Ring {
 	public Ring(int x, int y, int z) {
 		position = new GLVektor(x,y,z);
 		body = new GLTorus(position, 50, 7);
+		body.setzeFarbe(0.81, 0.33, 0.0);
 	}
 	public GLVektor getPosition() {}
 }
