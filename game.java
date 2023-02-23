85752e5367d94997aefbd9cfc119cfc3f4b061e5
Fakecrafter <Fakecrafter@protonmail.com>
2023-02-23 15:54:56 +0100

    Kollisionabfrage

--- game.java
+++ game.java
@@ -6,7 +6,6 @@ class Game {
 	private GLKamera cam;
 	private GLLicht light;
 	private int score;
-	private int highscore;
 	private GLTastatur keyboard;
 	private Plane plane;
 	private Ring[] rings;
@@ -23,16 +22,17 @@ class Game {
 		plane = new Plane();
 		rings = new Ring[10];
 		score = 0;
-		highscore = 0;
 		for(int i=0; i<10; i++) {
-			rings[i] = new Ring(i*20, 50, i*20);
+			rings[i] = new Ring(2000, 100, 2000);
 		}
 	}
 	public void main() {
+		// game loop
 		while (true) {
 			checkCollision();
 			checkKeyboard();
 			plane.run();
+			cam.setzeBlickpunkt(plane.getCenter());
 			Sys.warte(17);
 		}
 	}
@@ -44,6 +44,21 @@ class Game {
 		if (keyboard.rechts()){plane.headRight();}
 	}
 	void checkCollision() {
+		double distance;
+		GLVektor planePos = plane.getCenter();
+		// Kollision mit allen Ringen ueberpruefen
+		for(int i=0; i<rings.length; i++) {
+			GLVektor ringPos = rings[i].getCenter();
+			ringPos.subtrahiere(planePos);
+			// wie weit ist das Flugzeug vom Mittelpunkt eines Rings entfernt
+			distance = Math.sqrt(ringPos.gibX() * ringPos.gibX() + ringPos.gibY() * ringPos.gibY() + ringPos.gibZ() * ringPos.gibZ());
+
+			if(distance < 5) {
+				rings[i].loesche();
+				rings[i] = new Ring(2000, 100, 2000);
+				score++;
+			}
+		}
 	}
 }
 
